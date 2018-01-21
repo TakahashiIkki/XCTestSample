5cd69d52f2d070cc2bf995abfbaa90c50509a1b8
TakahashiIkki <new_ittyandesu@outlook.jp>
2018-01-21 11:59:51 +0900

    add testCode

--- TestSample/MathFunction.swift
+++ TestSample/MathFunction.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2018年 一騎高橋. All rights reserved.
 //
 
-import Foundation
+class MathFunction {
+    
+    /// 値を足して和を返す関数
+    ///
+    /// - Parameters:
+    ///   - addedValue: 被加数(足される数)
+    ///   - addValue:   加数(足す数)
+    /// - Returns: 被加数 と 加数 の和
+    func add(addedValue: Int, addValue: Int) -> Int {
+        return addedValue + addValue
+    }
+}
